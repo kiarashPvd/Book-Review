d525f4645d09a07eac609839acf1f337d2e9941c
mohkia82 <Mohkia82@gmail.com>
2023-11-29 00:50:52 +0330

    Update views.py

--- bookReview/views.py
+++ bookReview/views.py
@@ -1,3 +1,3 @@
 from django.http import HttpResponse
 def book_review(request):
-    return HttpResponse('salaaaaam')
+    return HttpResponse(' <h1>سلام به کیا توپیا خوش امدید </h1>(ورود ارمین و کسانی که ویزا ندارن ممنوع (به جز کوروش))') 
