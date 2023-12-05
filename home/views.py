4e8be215b4f032e7f5bade391b413ab7d4908710
mohkia82 <Mohkia82@gmail.com>
2023-12-05 17:59:34 +0330

    added general templates folder

--- home/views.py
+++ home/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
 def about_view(request):
-    return HttpResponse('در این مکان همه چیز ازاد است به جز اسلام و مواد مخدر و خلاف') 
+    return render(request, 'home/about.html')
 
 def contact_view(request):
-    return HttpResponse(' برای ورود مستقیما با پادشاه کیارش تماس بگیرید') 
+    return render(request, 'home/contact.html')
 # Create your views here.
