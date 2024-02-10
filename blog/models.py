5b3ff0750f64d1fe5f1093c0e0fd88b31af08eec
kiarash.pvd <Mohkia82@gmail.com>
2024-02-11 00:46:13 +0330

    import models
    import models in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 class post(models.Model):
-    
+    # image
+    # author
     title = models.CharField(max_length= 255)
     contex = models.TextField()
+    # tag
+    # category
+    counted_veiws = models.IntegerField() #(default=0)
+    status = models.BooleanField(default=False) 
+    published_date = models.DateTimeField(null = True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
