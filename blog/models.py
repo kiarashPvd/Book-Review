db90bdfe557cd58b827a972aa9b86f0db5126cb5
kiarash.pvd <Mohkia82@gmail.com>
2024-02-12 03:20:30 +0330

    giving values ​​to database variables

--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,11 @@ class post(models.Model):
     # image
     # author
     title = models.CharField(max_length= 255)
-    contex = models.TextField()
+    content = models.TextField()
     # tag
     # category
-    counted_veiws = models.IntegerField() #(default=0)
+    counted_veiws = models.IntegerField(default=0)
     status = models.BooleanField(default=False) 
     published_date = models.DateTimeField(null = True)
     created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    updated_date = models.DateTimeField(auto_now=True) #2024-02-11 22:54:57.872785
