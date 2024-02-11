7f128629c1e2ca932123e2521ff8f948dc20305d
kiarash.pvd <Mohkia82@gmail.com>
2024-02-11 03:47:40 +0330

    import models in home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class contact (models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
