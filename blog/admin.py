b9a003ef4e78125bcb7248d7d517373b43b8da95
kiarash.pvd <Mohkia82@gmail.com>
2024-02-12 22:44:14 +0330

    added PostAdmin class in blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import post
 # Register your models here.
-admin.site.register(post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    empty_value_display = '-empty-'
+    list_display = ('title','status','published_date','created_date','counted_veiws')
+    list_filter = ('status',)
+    ordering = ['created_date']
+    search_fields = ['title','content']
+admin.site.register(post,PostAdmin)
 
