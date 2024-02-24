477bd45182559d8079f86b6fb2d487d65411924e
kiarash.pvd <Mohkia82@gmail.com>
2024-02-24 23:19:02 +0330

    update blog models

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class post(models.Model):
-    # image
-    # author
+    image = models.ImageField
+    author = models.CharField(max_length=255)
     title = models.CharField(max_length= 255)
     content = models.TextField()
     # tag
