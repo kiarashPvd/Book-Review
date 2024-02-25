5534d6d61c20a93b50a18694c2c44f6131789947
kiarash.pvd <Mohkia82@gmail.com>
2024-02-25 23:58:35 +0330

    update blog home

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from .models import post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     empty_value_display = '-empty-'
-    list_display = ('title','status','published_date','created_date','counted_veiws')
+    list_display = ('title','status','author','published_date','created_date','counted_veiws')
     list_filter = ('status',)
     ordering = ['created_date']
     search_fields = ['title','content']
