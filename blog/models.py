65f009a4ca60d335a8718397b15d8563fc5a8c90
kiarash.pvd <payervand.kiarash@gmail.com>
2024-03-01 19:25:58 +0330

    create category model

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+class category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+
 class post(models.Model):
     Image = models.ImageField(upload_to="blog/",default='blog/NaNN.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null = True)   
     title = models.CharField(max_length= 255)
     content = models.TextField()
     # tag
-    # category
     counted_veiws = models.IntegerField(default=0)
+    category = models.models.ManyToManyField(category)
     status = models.BooleanField(default=False) 
     published_date = models.DateTimeField(null = True)
     created_date = models.DateTimeField(auto_now_add=True)
@@ -23,3 +35,4 @@ class post(models.Model):
         
     def __str__(self):
         return self.title
+    
