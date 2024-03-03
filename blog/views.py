a294e9f6c0c8bacc2a294fec6e10c5e5d2ccd0f5
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-03-03 18:42:51 +0330

    import ads in single and home blog

--- blog/views.py
+++ blog/views.py
@@ -13,8 +13,7 @@ def blog_single(request,pid):
     Post = get_object_or_404 (posts,pk=pid)
     next_post = posts.filter(created_date__lt=Post.created_date).last()
     previous_post = posts.filter(created_date__gt=Post.created_date).first()
-
-    context = {'post': Post,'next_post': next_post,'previous_post': previous_post}
+    context = {'post': Post,'next_post': next_post,'previous_post': previous_post,}
     return render(request, 'blog/blog-single.html',context)
 
 #def test(request,pid):
