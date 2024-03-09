04adc710ed784190d5763ec2d9dd77d5982458af
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-03-09 22:44:33 +0330

    import sort posts by category

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view, name= 'blog'),
     path('<int:pid>', blog_single, name= 'single'),
+    path('category/<str:cat_name>', blog_view, name= 'category'),
     path('test',test, name= 'test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from blog.models import post
 
 
-def blog_view(request):
+def blog_view(request,cat_name=None):
     posts = post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html',context)
 
@@ -15,6 +17,8 @@ def blog_single(request,pid):
     previous_post = posts.filter(created_date__gt=Post.created_date).first()
     context = {'post': Post,'next_post': next_post,'previous_post': previous_post,}
     return render(request, 'blog/blog-single.html',context)
+
+
 
 def test(request):#,pid
     #Post = get_object_or_404 (post,pk=pid)
