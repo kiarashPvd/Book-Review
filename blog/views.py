6aeb5665cf6a8e930314c861d5fb723f774ab014
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-03-11 03:34:04 +0330

    create paginator

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from blog.models import post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 def blog_view(request,**kwargs):
@@ -9,8 +10,17 @@ def blog_view(request,**kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username')!=None:
         posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html',context)
+
 
 def blog_single(request,pid):
     posts = post.objects.filter(status=1).order_by('created_date')
@@ -20,12 +30,15 @@ def blog_single(request,pid):
     context = {'post': Post,'next_post': next_post,'previous_post': previous_post,}
     return render(request, 'blog/blog-single.html',context)
 
+
 def blog_search(request):
     posts = post.objects.filter(status=1)
     if request.method == 'GET':
-        posts = posts.filter(content__contains=request.GET.get('s'))
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html',context)
+
 
 def test(request):#,pid
     #Post = get_object_or_404 (post,pk=pid)
