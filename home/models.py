427aecb1f06f4a9e3d18c6cb44aa5b5f9294fc54
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-03-20 14:54:37 +0330

    added age field in contact

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class contact (models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=255,null=True)
     message = models.TextField()
+    age = models.DateField(max_length=150,null = True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
