5a08ef54fd01028c44439014f57a6a62ac80cfb9
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-04-03 10:34:34 +0330

    install sitemap

--- bookReview/urls.py
+++ bookReview/urls.py
@@ -18,11 +18,21 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from home.sitemaps import StaticViewSitemap
+
+sitemaps ={
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    
+    path('sitemap.xml', sitemap,{'sitemaps': sitemaps},
+        name='dgango.contrib.sitemap.views.sitemap'),
+    path('robots.txt', include('robots.urls')), 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
