aa5a5ff3d749a4c5c4ff4412afc19130acc6bafc
kiarash.pvd <132096443+kiarashPvd@users.noreply.github.com>
2024-04-14 23:59:33 +0330

    add SummerNote in admin blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import post,category
+from django_semmernote.admin import SummerNoteAdmin
+
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummerNoteAdmin):
     date_hierarchy = "created_date"
     empty_value_display = '-empty-'
     list_display = ('title','status','author','published_date','created_date','counted_veiws')
     list_filter = ('status',)
     ordering = ['created_date']
     search_fields = ['title','content']
+    summernote_fields = ('content',)
     
 admin.site.register(post,PostAdmin)
 admin.site.register(category)
